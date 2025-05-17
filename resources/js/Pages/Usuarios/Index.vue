2cdfbc16741d0d8e90ed33ecbc24cb737e9cc32f
axlkun <axelcruz.dev@gmail.com>
2025-05-17 17:55:16 -0600

    add usuarios list view

---
+++ resources/js/Pages/Usuarios/Index.vue
@@ -0,0 +1,46 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import AppTable from '@/OwnComponents/Table.vue';
+import Container from '@/OwnComponents/Container.vue';
+import Card from '@/OwnComponents/Card.vue';
+import BreadCroumbs from '@/OwnComponents/BreadCroumbs.vue';
+
+const props = defineProps({
+    users: Object,
+});
+
+const headers = [
+    { name: "Nombre" },
+    { name: "Email" },
+    { name: "Registro" }
+];
+
+const breadcrumbs = [
+    {
+        label: "Usuarios"
+    }
+];
+
+</script>
+
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <BreadCroumbs :items="breadcrumbs"></BreadCroumbs>
+        </template>
+
+        <Container>
+
+            <Card class="mt-4">
+                <AppTable :headers="headers" :items="users">
+                    <tr v-for="user in users.data" :key="user.id">
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>{{ user.created_at }}</td>
+                    </tr>
+                </AppTable>
+            </Card>
+        </Container>
+
+    </AppLayout>
+</template>
