78c75bcfc92dd91e7950e665d82a4d6b6bfbe158
axlkun <axelcruz.dev@gmail.com>
2025-05-17 17:55:07 -0600

    add usuarios link

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -51,6 +51,10 @@ const logout = () => {
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+
+                                <NavLink :href="route('usuarios.index')" :active="route().current('usuarios.index')">
+                                    Usuarios
+                                </NavLink>
                             </div>
                         </div>
 
