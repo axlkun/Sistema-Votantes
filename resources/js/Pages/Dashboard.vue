2a0da3317839153c27953e3e1806aa94a49f9569
axlkun <axelcruz.dev@gmail.com>
2025-05-17 23:48:10 -0600

    styling dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -24,7 +24,7 @@ const breadcrumbs = [
 
                         <div
                             class="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="w-6 h-6">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="#800020" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 7.5a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.5 20.25c0-3.727 3.023-6.75 6.75-6.75h1.5c3.727 0 6.75 3.023 6.75 6.75v.75H4.5v-.75z" />
                               </svg>
                               
